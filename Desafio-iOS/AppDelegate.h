c05d0cdc8a82be34d21cbaaaee83a300e26385fd
Fabio Nogueira de Almeida <fna.contact@gmail.com>
2016-04-25 21:23:28 -0300

    Creating project and gitignore

---
+++ Desafio-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Desafio-iOS
+//
+//  Created by Fabio Nogueira on 25/04/16.
+//  Copyright © 2016 Fabio Nogueira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desafio-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Desafio-iOS
+//
+//  Created by Fabio Nogueira on 25/04/16.
+//  Copyright © 2016 Fabio Nogueira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
